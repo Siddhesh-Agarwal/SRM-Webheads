e9541631b73c527b3d1433b5b09c8cac6acdfd49
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2022-04-30 17:25:57 +0530

    added more files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'accent-1': '#1c2128',
+                'accent-2': '#2d333b',
+            }
+        }
+    }
+}

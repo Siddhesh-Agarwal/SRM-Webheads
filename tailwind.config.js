7c0eec82977dfaa1def2089082290eb833f28c2c
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2022-05-01 10:24:41 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                'accent-1': '#1c2128',
-                'accent-2': '#2d333b',
+                'accent-1': '#202b38',
+                'accent-2': '#161f27',
             }
         }
     }
